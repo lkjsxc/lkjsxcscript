1ee7ce668de6382b3c40460d6a50421f7a5165b8
lkjsxc <lkjsxc@gmail.com>
2025-04-13 10:23:30 +0000

    Add source file for compilation testing

--- src/lkjsxcscript.c
+++ src/lkjsxcscript.c
@@ -1,73 +1,185 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
 
-static struct termios term_orig;
+#define MEMORY_SIZE (1024 * 16)
+#define PORT 8080
+#define ISDEBUG 1
 
-void term_deinit() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &term_orig);
+enum result {
+    RESULT_OK,
+    RESULT_ERR,
+};
+
+enum type {
+    INST_NULL,
+    INST_NOP,
+    INST_PUSH_CONST,
+    INST_PUSH_VAR,
+    INST_GLOBAL_GET,
+    INST_GLOBAL_SET,
+    INST_CALL,
+    INST_RETURN,
+    INST_JUMP,
+    INST_JZE,
+    INST_OR,
+    INST_AND,
+    INST_NOT,
+    INST_EQ,
+    INST_NE,
+    INST_LT,
+    INST_GT,
+    INST_ADD,
+    INST_SUB,
+    INST_MUL,
+    INST_DIV,
+    INST_MOD,
+    INST_READ,
+    INST_WRITE,
+    LABEL_OPEN,
+    LABEL_CLOSE,
+    INST_ACCEPT,
+    INST_USLEEP,
+};
+
+struct vec {
+    char* data;
+    int size;
+};
+
+struct node {
+    struct vec* token;
+    struct node* next;
+    struct node* lhs;
+    struct node* rhs;
+    int64_t value;
+    enum type type;
+};
+
+struct compiler {
+    char code[MEMORY_SIZE / 32];
+    char src[MEMORY_SIZE / 32];
+    struct vec token[MEMORY_SIZE / 32];
+    struct node node[MEMORY_SIZE / 32];
+    struct node map_key[MEMORY_SIZE / 32];
+    struct node map_value[MEMORY_SIZE / 32];
+};
+
+union mem {
+    int64_t i64[MEMORY_SIZE / sizeof(int64_t)];
+    struct compiler compiler;
+};
+
+static union mem mem;
+
+enum result compile_readsrc(char* path) {
+    FILE* file = fopen(path, "r");
+    if (!file) {
+        fprintf(stderr, "Error opening file: %s\n", path);
+        return RESULT_ERR;
+    }
+    size_t bytes_read = fread(mem.compiler.src, 1, sizeof(mem.compiler.src), file);
+    fclose(file);
+    mem.compiler.src[bytes_read] = '\n';
+    mem.compiler.src[bytes_read+1] = '\0';
+    return RESULT_OK;
 }
 
-void term_init() {
-    static struct termios term_new;
-    tcgetattr(STDIN_FILENO, &term_orig);
-    term_new = term_orig;
-    term_new.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-    term_new.c_oflag &= ~(OPOST);
-    term_new.c_cflag |= (CS8);
-    term_new.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-    term_new.c_cc[VMIN] = 0;
-    term_new.c_cc[VTIME] = 1;
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &term_new);
+enum result compile_tokenize() {
+    struct vec* token_end = mem.compiler.token;
+    char* base = mem.compiler.src;
+    char* itr = base;
+    for (; *itr != '\0'; itr++) {
+        if (*itr == ' ' || *itr == '\n' || *itr == '\t') {
+            if (base != itr) {
+                *(token_end++) = (struct vec){.data = base, .size = itr - base};
+            }
+            base = itr + 1;
+            continue;
+        }
+        if (*itr == '+' || *itr == '-' || *itr == '*' || *itr == '/' || *itr == '%' ||
+            *itr == '=' || *itr == '!' || *itr == '<' || *itr == '>' || *itr == '&' ||
+            *itr == '|' || *itr == '^' || *itr == '~' || *itr == '(' || *itr == ')' ||
+            *itr == '{' || *itr == '}' || *itr == '[' || *itr == ']' || *itr == ';' ||
+            *itr == ',' || *itr == ':' || *itr == '.' || *itr == '?' || *itr == '!' ||
+            *itr == '\'' || *itr == '\"' || *itr == '`' || *itr == '\\') {
+            if (base != itr) {
+                *(token_end++) = (struct vec){.data = base, .size = itr - base};
+            }
+            *(token_end++) = (struct vec){.data = itr, .size = 1};
+            base = itr + 1;
+            continue;
+        }
+    }
+    *token_end = (struct vec){.data = NULL, .size = 0};
 }
 
-void vm_compile_tokenize() {
+enum result compile_parse() {
 }
 
-void vm_compile_parse() {
+enum result compile_analyze() {
 }
 
-void vm_compile_analyze() {
+enum result compile_optimize() {
 }
 
-void vm_compile_optimize() {
+enum result compile_generate() {
 }
 
-void vm_compile_generate() {
+enum result compile(char* path) {
+    if (compile_readsrc(path) == RESULT_ERR) {
+        fprintf(stderr, "Error reading source file: %s\n", path);
+        return RESULT_ERR;
+    }
+    if (compile_tokenize() == RESULT_ERR) {
+        fprintf(stderr, "Error tokenizing source file: %s\n", path);
+        return RESULT_ERR;
+    }
+    if (compile_parse() == RESULT_ERR) {
+        fprintf(stderr, "Error parsing source file: %s\n", path);
+        return RESULT_ERR;
+    }
+    if (compile_analyze() == RESULT_ERR) {
+        fprintf(stderr, "Error analyzing source file: %s\n", path);
+        return RESULT_ERR;
+    }
+    if (compile_optimize() == RESULT_ERR) {
+        fprintf(stderr, "Error optimizing source file: %s\n", path);
+        return RESULT_ERR;
+    }
+    if (compile_generate() == RESULT_ERR) {
+        fprintf(stderr, "Error generating code for source file: %s\n", path);
+        return RESULT_ERR;
+    }
+    return RESULT_OK;
 }
 
-void vm_compile() {
+enum result execute() {
 }
 
-void vm_execute() {
-}
+int main(int argc, char** argv) {
 
-void vm_deinit() {
-}
-
-void vm_init() {
-}
-
-void deinit() {
-    vm_deinit();
-    term_deinit();
-}
-
-void init() {
-    term_init();
-    vm_init();
-}
-
-void run() {
-    vm_compile();
-    vm_execute();
-}
-
-int main() {
-    init();
-    run();
-    deinit();
+#if ISDEBUG
+    if (compile("./lkjsxcscript_src") == RESULT_ERR) {
+        fprintf(stderr, "Compilation failed\n");
+        return 1;
+    }
+#else
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <source_file>\n", argv[0]);
+        return 1;
+    }
+    if (compile(argv[1]) == RESULT_ERR) {
+        fprintf(stderr, "Compilation failed\n");
+        return 1;
+    }
+#endif
+    if (execute() == RESULT_ERR) {
+        fprintf(stderr, "Execution failed\n");
+        return 1;
+    }
     return 0;
 }
